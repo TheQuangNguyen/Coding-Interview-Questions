6c0e6804ae974d8129b8638a61028997875bcd5f
Quang Nguyen <quangn2595@gmail.com>
2019-12-20 19:47:31 -0800

    finished solving bottom up level traversal for binary tree problem

--- src/main/java/Easy/BottomUpLevelTraversalBinaryTree.java
+++ src/main/java/Easy/BottomUpLevelTraversalBinaryTree.java
@@ -1,5 +1,7 @@
 package Easy;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Stack;
 
@@ -21,10 +23,38 @@ public class BottomUpLevelTraversalBinaryTree {
     // ]
 
     public List<List<Integer>> levelOrderBottom(TreeNode root) {
-        Stack<Integer> stack = new Stack<>();
+        int height = height(root);
+        int index = 0;
+        List<Integer>[] result = new ArrayList[height];
+        levelOrderBottom(root, height, index, result);
+        return Arrays.asList(result);
+    }
 
-        stack.push(root.val);
-        
+    public void levelOrderBottom(TreeNode currentNode, int height, int index, List<Integer>[] result) {
+        if (currentNode == null) {
+            return;
+        }
+        index++;
+        levelOrderBottom(currentNode.left, height, index, result);
+        levelOrderBottom(currentNode.right, height, index, result);
+
+        int currentLevel = height - index;
+
+        if (result[currentLevel] == null) {
+            List<Integer> currentLevelList = new ArrayList<>();
+            currentLevelList.add(currentNode.val);
+            result[currentLevel] = currentLevelList;
+        } else {
+            result[currentLevel].add(currentNode.val);
+        }
+    }
+
+    public int height(TreeNode currentNode) {
+        if (currentNode == null) {
+            return 0;
+        }
+
+        return Math.max(height(currentNode.left), height(currentNode.right)) + 1;
     }
 
     public class TreeNode {
@@ -33,4 +63,10 @@ public class BottomUpLevelTraversalBinaryTree {
         TreeNode right;
         TreeNode(int x) { val = x; }
     }
+
+    // Runtime: 0 ms, faster than 100.00%
+    // Memory Usage: 36.2 MB, less than 100.00%
+
+    // Time complexity is O(n) where n is number of nodes in the tree
+    // Space complexity is O(n) for heap since we have to store all the nodes' values and O(h) for stack where h is the height of the tree
 }
